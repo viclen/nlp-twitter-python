abf2a278fcf787dfbebdbdd1fe08b5a916d49c0a
Victor <victorpbi5@hotmail.com>
2020-07-26 14:20:59 -0400

    EN dataset and model

--- predictor.py
+++ predictor.py
@@ -6,20 +6,19 @@ from ktrain import text
 from TweetListener import TweetListener
 from sklearn.model_selection import train_test_split
 
+
 def create():
     print("Preparing dataset")
-    dataset = pd.read_csv("./drive/My Drive/NLP/imdb-reviews-pt-br.csv")
-    X = dataset.drop('text_en', axis=1).rename(columns={"text_pt": "text"})
-    y = dataset.sentiment
-    data_train, data_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    # dataset = pd.read_csv("./drive/My Drive/NLP/ktrain/Train3Classes.csv", ";")
-    # dataset.sentiment = dataset.sentiment.replace([0, 2, 1], ['neg', 'neu', 'pos'])
-    # data_train = dataset.drop(labels=['id', 'query_used', 'tweet_date'], axis=1).rename(columns={ "tweet_text": "text" })
+    dataset = pd.read_csv("./drive/My Drive/NLP/EN/dataset.csv",
+                          ",", encoding='ISO-8859-1')  # pd.concat(frames)
+    dataset.columns = ['id', 'sentiment', 'text']
+    dataset = dataset.drop(labels=['id'], axis=1)
 
-    # dataset = pd.read_csv("./drive/My Drive/NLP/ktrain/Test3Classes.csv", ";")
-    # dataset.sentiment = dataset.sentiment.replace([0, 2, 1], ['neg', 'neu', 'pos'])
-    # data_test = dataset.drop(labels=['id', 'query_used', 'tweet_date'], axis=1).rename(columns={ "tweet_text": "text" })
+    dataset.sentiment = dataset.sentiment.replace([0, 0.5, 1], ['neg', 'neu', 'pos'])
+    
+    data_train = dataset[(dataset.index>np.percentile(dataset.index, 0)) & (dataset.index<=np.percentile(dataset.index, 80))]
+    data_test = dataset[(dataset.index>np.percentile(dataset.index, 81)) & (dataset.index<=np.percentile(dataset.index, 100))]
 
     (X_train, y_train), (X_test, y_test), preprocess = text.texts_from_df(
         train_df=data_train,
@@ -49,7 +48,7 @@ def create():
     )
 
     print("Loading saved model")
-    learner.load_model('./drive/My Drive/NLP/ktrain/model')
+    learner.load_model('./drive/My Drive/NLP/EN/model')
 
     print("Creating predictor")
     return ktrain.get_predictor(learner.model, preprocess)
